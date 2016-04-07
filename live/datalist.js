866c4227d3e46456b36400fb2ec6522f1d5a4a2f
Skyle <skyle520@163.com>
2016-04-07 19:13:26 +0800

    添加一个取数据函数

--- live/datalist.js
+++ live/datalist.js
@@ -1,3 +1,19 @@
 function dataist10() {<TRS_DOCUMENTS num="10">
   document.writeln('<dt class="ht_title"><TRS_DOCUMENT field="doctitle" num="31" target="_blank">文档标题</TRS_DOCUMENT></dt>');</TRS_DOCUMENTS>
 }
+
+function inverstmentTools() {
+  document.writeln('<ul>');<TRS_DOCUMENTS num="16" id="证券_投资工具">
+  document.writeln('    <li><TRS_DOCUMENT field="doctitle" target="_blank">文档标题</TRS_DOCUMENT></li>');</TRS_DOCUMENTS>
+  document.writeln('</ul>');
+}
+
+function tradingTips() {
+  document.writeln('<ul class="title">');<TRS_DOCUMENTS num="6" id="证券_推荐_交易提示">
+  document.writeln('    <li><span class="kxbt"><TRS_DOCUMENT field="doctitle" target="_blank" num="40" TRUNCATEDFLAG="">文档标题</TRS_DOCUMENT></span></li>');</TRS_DOCUMENTS>
+  document.writeln('</ul>');
+}
+
+function companyNews10() {<TRS_DOCUMENTS num="10" id="证券_公司新闻">
+  document.writeln('<dt class="ht_title1"><TRS_DOCUMENT field="doctitle" num="31" target="_blank">文档标题</TRS_DOCUMENT></dt>');</TRS_DOCUMENTS>
+}
