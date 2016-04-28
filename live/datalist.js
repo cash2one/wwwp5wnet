1ad8355830abfd1209554d1b1ebca24b7ad006a3
Skyle <skyle520@163.com>
2016-04-28 10:46:35 +0800

    直播频道下的页面修改。

--- live/datalist.js
+++ live/datalist.js
@@ -4,16 +4,16 @@ function dataist10() {<TRS_DOCUMENTS num="10">
 
 function inverstmentTools() {
   document.writeln('<ul>');<TRS_DOCUMENTS num="16" id="证券_投资工具">
-  document.writeln('    <li><TRS_DOCUMENT field="doctitle" target="_blank">文档标题</TRS_DOCUMENT></li>');</TRS_DOCUMENTS>
+  document.writeln('    <li><TRS_DOCUMENT field="doctitle" target="_blank" UrlIsAbs="true">文档标题</TRS_DOCUMENT></li>');</TRS_DOCUMENTS>
   document.writeln('</ul>');
 }
 
 function tradingTips() {
   document.writeln('<ul class="title">');<TRS_DOCUMENTS num="6" id="证券_推荐_交易提示">
-  document.writeln('    <li><span class="kxbt"><TRS_DOCUMENT field="doctitle" target="_blank" num="40" TRUNCATEDFLAG="">文档标题</TRS_DOCUMENT></span></li>');</TRS_DOCUMENTS>
+  document.writeln('    <li><span class="kxbt"><TRS_DOCUMENT field="doctitle" target="_blank" num="40" TRUNCATEDFLAG="" UrlIsAbs="true">文档标题</TRS_DOCUMENT></span></li>');</TRS_DOCUMENTS>
   document.writeln('</ul>');
 }
 
 function companyNews10() {<TRS_DOCUMENTS num="10" id="证券_公司新闻">
-  document.writeln('<dt class="ht_title1"><TRS_DOCUMENT field="doctitle" num="31" target="_blank">文档标题</TRS_DOCUMENT></dt>');</TRS_DOCUMENTS>
+  document.writeln('<dt class="ht_title1"><TRS_DOCUMENT field="doctitle" num="31" target="_blank" UrlIsAbs="true">文档标题</TRS_DOCUMENT></dt>');</TRS_DOCUMENTS>
 }
