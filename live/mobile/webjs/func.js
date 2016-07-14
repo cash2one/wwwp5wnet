413381210db359d126d8f0e65f11575cf8b06e70
Skyle <skyle520@163.com>
2016-07-14 16:09:59 +0800

    直播栏目： 1.新增媒体说明会分类页面； 2.手机直播页面修改，修改视频调用方式为gensee SDK。

--- live/mobile/webjs/func.js
+++ live/mobile/webjs/func.js
@@ -1,4 +1,5 @@
 $(function () {
+    videoGensee();
     $('#refresh').bind('click', function () {
         window.location.reload();
     });
@@ -41,17 +42,48 @@ $(function () {
     //     });
     // });
 
-    liveSrc = $.trim(liveSrc);
-    liveTitle = $.trim(liveTitle);
-    if (!(liveSrc == '' || liveSrc == null || liveSrc == undefined)) {
-        //var liveBoxHtml = '<div class="liveBox"><video src="' + liveSrc + '" width="100%" height="" controls="controls"></video></div>';
-        var liveBoxHtml = new Array();
-        liveBoxHtml.push('<div class="temp-wrap video">');
-        liveBoxHtml.push('    <div class="mbody" style="background-color: #000;">');
-        liveBoxHtml.push('        <video src="' + liveSrc + '" width="100%" height="auto" controls></video>');
-        liveBoxHtml.push('    </div>');
-        liveBoxHtml.push('    <h4 class="title f14 tc white-bg">' + liveTitle + '</h4>');
-        liveBoxHtml.push('</div>');
-        $('.live-intro').after(liveBoxHtml.join(''));
-    }
+    // liveSrc = $.trim(liveSrc);
+    // liveTitle = $.trim(liveTitle);
+    // if (!(liveSrc == '' || liveSrc == null || liveSrc == undefined)) {
+    //     //var liveBoxHtml = '<div class="liveBox"><video src="' + liveSrc + '" width="100%" height="" controls="controls"></video></div>';
+    //     var liveBoxHtml = new Array();
+    //     liveBoxHtml.push('<div class="temp-wrap video">');
+    //     liveBoxHtml.push('    <div class="mbody" style="background-color: #000;">');
+    //     liveBoxHtml.push('        <video src="' + liveSrc + '" width="100%" height="auto" controls></video>');
+    //     liveBoxHtml.push('    </div>');
+    //     liveBoxHtml.push('    <h4 class="title f14 tc white-bg">' + liveTitle + '</h4>');
+    //     liveBoxHtml.push('</div>');
+    //     $('.live-intro').after(liveBoxHtml.join(''));
+    // }
 });
+
+var videoGensee = function () {
+    try {
+        liveCode = $.trim(liveCode);
+        vodCode = $.trim(vodCode);
+        liveTitle = $.trim(liveTitle);
+    } catch (e) {
+        return;
+    }
+    var videoHtml = new Array();
+    if (!(vodCode == '' || vodCode == null || vodCode == undefined)) {
+        videoHtml.push('<div class="temp-wrap video">');
+        videoHtml.push('    <div class="mbody" style="background-color: #000;">');
+        videoHtml.push('        <gs:video-vod site="jcsx.gensee.com" ctx="webcast" ownerid="' + vodCode + '" py="0" bar="true"/>');
+        videoHtml.push('    </div>');
+        videoHtml.push('    <h4 class="title f14 tc white-bg">' + liveTitle + '</h4>');
+        videoHtml.push('</div>');
+        $('.live-intro').after(videoHtml.join(''));
+        return;
+    }
+    if (!(liveCode == '' || liveCode == null || liveCode == undefined)) {
+        videoHtml.push('<div class="temp-wrap video">');
+        videoHtml.push('    <div class="mbody" style="background-color: #000;">');
+        videoHtml.push('        <gs:video-live site="jcsx.gensee.com" ctx="webcast" ownerid="' + liveCode + '" py="1" bar="true"/>');
+        videoHtml.push('    </div>');
+        videoHtml.push('    <h4 class="title f14 tc white-bg">' + liveTitle + '</h4>');
+        videoHtml.push('</div>');
+        $('.live-intro').after(videoHtml.join(''));
+        return;
+    }
+}
